1c28a00c4b451445a0c55813501a3d7a784e83a7
David Wu <david204code@gmail.com>
2019-09-30 14:09:21 +0100

    Added the links onto the _header partial and configure the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
 
   root 'page#home'
 
-  get 'page/home'
-  get 'page/photo'
-  get 'page/location'
-  get 'page/riders'
-  get 'page/contest'
+  get '/home', to: 'page#home'
+  get '/photo', to: 'page#photo'
+  get '/location', to: 'page#location'
+  get '/riders', to: 'page#riders'
+  get 'contest', to: 'page#contest'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
